d1f6329548848eed6bf2e66794f0da4d72708da7
GenerelSchwerz <xXSG.KingXx@gmail.com>
2022-01-16 15:45:00 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import utilPlugin from "@nxg-org/mineflayer-util-plugin";
+import { EntityTracker } from "./entityTracker";
+import { ProjectileTracker } from "./projectileTracker";
+import { Bot } from "mineflayer";
+import { Entity } from "prismarine-entity";
+
+declare module "mineflayer" {
+    interface Bot {
+        tracker: EntityTracker
+        projectiles: ProjectileTracker;
+
+    }
+    interface BotEvents {
+        attackedTarget: (target: Entity) => void;
+        stoppedAttacking: () => void;
+        startedAttacking: (target: Entity) => void;
+        targetBlockingUpdate: (target: Entity, blocking: boolean) => void;
+    }
+}
+
+export default function plugin(bot: Bot) {
+    if (!bot.util) bot.loadPlugin(utilPlugin);
+    bot.tracker = new EntityTracker(bot);
+    bot.projectiles = new ProjectileTracker(bot);
+}
+
+export { EntityTracker };
